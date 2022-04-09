f76d5f8653224c18b1004af2b22ed620a546c7ef
André Alves <andreluialves@hotmail.com>
2022-04-08 23:18:20 -0300

    refactor: correção de nomos dos componentes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import Feadback from './pages/Feadback';
+import Feedback from './pages/Feedback';
 import Login from './pages/Login';
 import Questions from './pages/Questions';
 import Settings from './pages/Settings';
@@ -9,7 +9,7 @@ export default function App() {
   return (
     <div>
       <Switch>
-        <Route path="/feadback" component={ Feadback } />
+        <Route path="/feadback" component={ Feedback } />
         <Route path="/settings" component={ Settings } />
         <Route path="/questions" component={ Questions } />
         <Route exact path="/" component={ Login } />
