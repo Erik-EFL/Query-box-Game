2f6eef32dcdcc3a36b514a02d2fb415a7e839df4
Helder Medeiros <hmedeiros211@gmail.com>
2022-04-13 12:57:06 -0300

    ajustes visuais

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -3,15 +3,29 @@ import React from 'react';
 import { connect } from 'react-redux';
 import styles from '../Css/Timer.module.css';
 import timerIcon from '../Css/assets/timer.png';
+import timerZero from '../Css/assets/timerZero.gif';
 import '../Css/Questions.module.css';
 
 class Timer extends React.Component {
+  timerAnimation = () => {
+    const { timerQuestion } = this.props;
+    if (timerQuestion > 0) {
+      return (
+        <img className={ styles.timer_icon } src={ timerIcon } alt="timer" />
+      );
+    }
+    return (
+      <img className={ styles.timer_icon } src={ timerZero } alt="timer" />
+    );
+  }
+
   render() {
     const { timerQuestion } = this.props;
+    const time = (`00${timerQuestion}`).slice(Number('-2'));
     return (
       <div className={ styles.timer_container }>
-        <span className={ styles.timer_text }>{ timerQuestion }</span>
-        <img className={ styles.timer_icon } src={ timerIcon } alt="timer" />
+        <span className={ styles.timer_text }>{ time }</span>
+        {this.timerAnimation()}
       </div>
     );
   }
